7021c11aaa10919652d0d263c41cd65ac91237a7
jeyangg <jeyang.980619@gmail.com>
2024-11-29 14:28:21 -0500

    Init code generation

---
+++ 3_CodeGen/codegen.py
@@ -0,0 +1,111 @@
+import sys
+import ast_node
+import lexer_2
+
+class MIPSCodeGenerator:
+    def __init__(self):
+        self.code = []
+        self.label_counter = 0
+
+    def generate_label(self, base="label"):
+        label = f"{base}_{self.label_counter}"
+        self.label_counter += 1
+        return label
+
+    def process_ast(self, node):
+        if isinstance(node, ast_node.FuncDefNode):
+            self.code.append(f"\n# Function {node.name}")
+            self.code.append(f"{node.name}:")
+            for stmt in node.body:
+                self.process_ast(stmt)
+            self.code.append("jr $ra  # Return from function")
+        elif isinstance(node, ast_node.AssignNode):
+            self.process_ast(node.expr)
+            self.code.append(f"sw $v0, {node.var.name}")
+        elif isinstance(node, ast_node.BinaryOpNode):
+            self.process_ast(node.left)
+            self.code.append("move $t1, $v0")
+            self.process_ast(node.right)
+            if node.operator == "+":
+                self.code.append("add $v0, $t1, $v0")
+            elif node.operator == "-":
+                self.code.append("sub $v0, $t1, $v0")
+            elif node.operator == "*":
+                self.code.append("mul $v0, $t1, $v0")
+            elif node.operator == "/":
+                self.code.append("div $v0, $t1, $v0")
+        elif isinstance(node, ast_node.NumberNode):
+            self.code.append(f"li $v0, {node.value}")
+        elif isinstance(node, ast_node.IdentifierNode):
+            self.code.append(f"lw $v0, {node.name}")
+        elif isinstance(node, ast_node.IfNode):
+            self.process_ast(node.condition)
+            false_label = self.generate_label("false")
+            self.code.append(f"beq $v0, $zero, {false_label}")
+            for stmt in node.body:
+                self.process_ast(stmt)
+            if node.else_body:
+                end_label = self.generate_label("end")
+                self.code.append(f"j {end_label}")
+                self.code.append(f"{false_label}:")
+                for stmt in node.else_body:
+                    self.process_ast(stmt)
+                self.code.append(f"{end_label}:")
+            else:
+                self.code.append(f"{false_label}:")
+        elif isinstance(node, ast_node.WhileNode):
+            start_label = self.generate_label("start")
+            end_label = self.generate_label("end")
+            self.code.append(f"{start_label}:")
+            self.process_ast(node.condition)
+            self.code.append(f"beq $v0, $zero, {end_label}")
+            for stmt in node.body:
+                self.process_ast(stmt)
+            self.code.append(f"j {start_label}")
+            self.code.append(f"{end_label}:")
+        elif isinstance(node, ast_node.PrintNode):
+            self.process_ast(node.expr)
+            self.code.append("li $v0, 1  # Print integer syscall")
+            self.code.append("syscall")
+
+    def get_code(self):
+        return "\n".join(self.code)
+
+
+class Pipeline:
+    def __init__(self, source_code):
+        self.source_code = source_code
+        self.lexer = lexer_2.Lexer()
+        self.parser = None
+        self.generator = MIPSCodeGenerator()
+
+    def process(self):
+        # Step 1: Lexical Analysis
+        tokens = self.lexer.tokenize(self.source_code)
+        self.parser = Parser(tokens)
+
+        # Step 2: Syntactic Analysis
+        ast = self.parser.parse()
+
+        # Step 3: Code Generation
+        for node in ast:
+            self.generator.process_ast(node)
+
+        # Step 4: Output the generated code
+        generated_code = self.generator.get_code()
+        with open("output.asm", "w") as output_file:
+            output_file.write(generated_code)
+        print("Generated MIPS code saved to output.asm")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python mips_codegen.py <input_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    with open(input_file, "r", encoding="utf-8") as f:
+        source_code = f.read()
+
+    pipeline = Pipeline(source_code)
+    pipeline.process()
---
+++ 3_CodeGen/lexer_2.py
@@ -0,0 +1,287 @@
+import pdb
+import re
+import enum
+import sys
+
+class LexerState(enum.Enum):
+    START = 'START'
+    IN_KEYWORD = 'IN_KEYWORD'
+    IN_IDENTIFIER = 'IN_IDENTIFIER'
+    IN_NUMBER = 'IN_NUMBER'
+    IN_OPERATOR = 'IN_OPERATOR'
+    IN_DELIMITER = 'IN_DELIMITER'
+    IN_STRING = 'IN_STRING'
+    ERROR = 'ERROR'
+
+class TokenType(enum.Enum):
+    KEYWORD = 'KEYWORD'
+    IDENTIFIER = 'IDENTIFIER'
+    NUMBER = 'NUMBER'
+    OPERATOR = 'OPERATOR'
+    DELIMITER = 'DELIMITER'
+    STRING = 'STRING'
+    COMMENT = 'COMMENT'
+    BOOL = 'BOOL'
+    NULL = 'NULL'
+    ERROR = 'ERROR'
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return ('Token({}, {})'.format(self.type, self.value))
+
+
+class Lexer:
+    def __init__(self):
+        self.state = LexerState.START
+        self.input = ""
+        self.position = 0       # for simple string input
+        self.line = 1           # for code with multiple lines
+        self.column = 1
+        self.hana_keywords = ["함수", "만약에", "만약", "아니면", "동안에", "반환", "출력", "진실", "거짓", "널"]
+        self.hana_logical = ["그리고", "이거나"]
+        self.hana_list = ["배열", "길이", "추가", "뽑기", "확장", "정렬"]
+        self.hana_dictionary = ["딕셔너리", "키", "아이템"]
+        self.hana_math = ["랜덤", "절댓값", "최소값", "최대값"]
+        self.hana_delimiter = []
+
+
+    def lookahead(self):
+        # reach to the end of the input
+        if self.position >= len(self.input): 
+            return None
+
+        char = self.input[self.position]
+        self.position += 1
+        self.column += 1
+
+        # reach to the end of the line
+        if char == '\n':
+            self.line += 1
+            self.column = 1
+        return char
+
+
+    def transition_state(self, new_state):
+        # print("Transitioning from {} to {}".format(self.state, new_state))
+        self.state = new_state
+
+
+    def tokenize(self, input_string):
+        self.input = input_string
+        tokens = []
+        while True:
+            token = self.tokenize_nxt()
+            if token is None:
+                break
+            tokens.append(token)
+        return tokens
+    
+
+    def tokenize_nxt(self):
+        char = self.lookahead()   
+        if char is None:
+            return None
+        
+        if self.state == LexerState.START:
+            if char.isspace():
+                return self.tokenize_nxt()  # Skip whitespace
+            elif char.isdigit():
+                self.transition_state(LexerState.IN_NUMBER)
+                return self.handle_digit(char)
+            elif char.isalpha():
+                self.transition_state(LexerState.IN_IDENTIFIER)
+                return self.handle_identifier(char)
+            elif char == '"':
+                self.transition_state(LexerState.IN_STRING)
+                return self.handle_string(char)
+            elif char == '#':
+                self.transition_state(LexerState.IN_STRING)
+                return self.handle_comment(char)
+            elif char in '+-*=!<>%/':
+                self.transition_state(LexerState.IN_OPERATOR)
+                return self.handle_operator(char)
+            elif char in '(){}[],:.':
+                self.transition_state(LexerState.IN_DELIMITER)
+                return self.handle_delimiter(char) 
+            else:
+                self.transition_state(LexerState.ERROR)
+                return Token(TokenType.ERROR, "Unexpected character: {} at line {}, column {}".format(char, self.line, self.column))
+
+        elif self.state == LexerState.IN_NUMBER:
+            return self.handle_digit(char)
+        
+        elif self.state == LexerState.IN_IDENTIFIER:
+            return self.handle_identifier(char)
+        
+        elif self.state == LexerState.IN_STRING:
+            return self.handle_string(char)
+        
+        elif self.state == LexerState.IN_OPERATOR:
+            self.transition_state(LexerState.START)
+            return self.handle_operator(char)
+
+        elif self.state == LexerState.IN_DELIMITER:
+            self.transition_state(LexerState.START)
+            return self.handle_delimiter(char)
+        
+        self.transition_state(LexerState.ERROR)
+        return Token(TokenType.ERROR, "Unexpected character: {}".format(char))
+
+
+    def handle_digit(self, digit_src):
+        value = digit_src
+        dot_encountered = False
+        is_error = False  # Flag to indicate if there's an error in the digit sequence
+
+        while True:
+            if self.position < len(self.input):
+                char = self.input[self.position]
+            else:
+                char = None
+
+            if char is None:
+                break
+
+            if char == '.' and not dot_encountered:
+                dot_encountered = True
+                self.position += 1
+                next_char = self.input[self.position] if self.position < len(self.input) else None
+                if next_char == '.':
+                    self.transition_state(LexerState.START)
+                    self.position -= 1
+                    return Token(TokenType.NUMBER, value)
+                elif next_char.isdigit():
+                    value += '.'
+                else:
+                    self.transition_state(LexerState.START)
+                    return Token(TokenType.NUMBER, value)
+            elif char == '.' and dot_encountered:
+                self.transition_state(LexerState.START)
+                return Token(TokenType.NUMBER, value)
+            elif char.isdigit():
+                value += self.lookahead()
+            elif char.isalpha():
+                value += self.lookahead()
+                is_error = True  # Mark this as an error since a digit was followed by a letter
+            else:
+                self.transition_state(LexerState.START)
+                break
+
+        if is_error:
+            self.transition_state(LexerState.START)
+            return Token(TokenType.IDENTIFIER, value)
+
+        return Token(TokenType.NUMBER, value)
+
+
+    def handle_operator(self, op_src):
+        value = op_src
+        next_char = self.lookahead()
+        if next_char is not None and next_char in '=<>*': 
+            value += next_char
+        self.transition_state(LexerState.START)
+        return Token(TokenType.OPERATOR, value)
+    
+
+    def handle_string(self, str_src):
+        value = str_src
+        while True:
+            char = self.lookahead()
+            if char is None:
+                self.transition_state(LexerState.START)
+                break
+
+            value += char
+            if char == '"':
+                self.transition_state(LexerState.START)
+                break
+        return Token(TokenType.STRING, value)
+    
+
+    def handle_comment(self, str_src):
+        value = str_src
+        while self.position < len(self.input):
+            char = self.input[self.position]
+            if char == '\n':
+                self.transition_state(LexerState.START)
+                break
+            value += self.lookahead()
+        return Token(TokenType.COMMENT, value)
+    
+    
+    def handle_delimiter(self, delimiter):
+        start_column = self.column - 1
+        if delimiter in '({[':
+            self.hana_delimiter.append((delimiter, self.line, start_column))
+        elif delimiter in ')}]':
+            if self.hana_delimiter:
+                last_delimiter = self.hana_delimiter[-1][0]
+                if (last_delimiter == '(' and delimiter == ')') or \
+                   (last_delimiter == '{' and delimiter == '}') or \
+                   (last_delimiter == '[' and delimiter == ']'):
+                    self.hana_delimiter.pop()
+        self.transition_state(LexerState.START)
+        return Token(TokenType.DELIMITER, delimiter)
+    
+
+    def handle_identifier(self, id_src):
+        value = id_src
+        is_error = False  # Flag to indicate if there's an error in the identifier
+
+        while True:
+            if self.position < len(self.input):
+                char = self.input[self.position]
+            else:
+                char = None
+
+            if char is None or not (char.isalnum() or char == '_'):
+                self.transition_state(LexerState.START)
+                break
+
+            # Check if a digit immediately follows a Korean character, which should raise an error
+            if char.isdigit() and not value[-1].isdigit() and not value[-1] == '_':
+                is_error = True  # Mark this as an error
+            value += self.lookahead()
+
+        # Check if the identifier is a keyword or falls into other predefined categories
+        if value in self.hana_keywords + self.hana_list + self.hana_dictionary + self.hana_math:
+            return Token(TokenType.KEYWORD, value)
+        elif value in self.hana_logical:
+            return Token(TokenType.OPERATOR, value)
+
+        return Token(TokenType.IDENTIFIER, value)
+
+
+def main(input_file):
+    if len(sys.argv) != 2:
+        print("Usage: python3.11 lexer.py <input_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    try:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            source_code = f.read()
+
+        # print(source_code)
+        lexer = Lexer()
+        tokens = lexer.tokenize(source_code)
+        for token in tokens:
+            print(token)
+
+    except FileNotFoundError:
+        print("Error: File {} not found.".format(input_file))
+        sys.exit(1)
+    except Exception as e:
+        print("An error occurred: {}".format(e))
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
